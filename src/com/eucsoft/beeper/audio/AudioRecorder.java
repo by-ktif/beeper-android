b9ffd061a168b3da4127596fd1ea8cdab01ad698
dimhold <dimhold@gmail.com>
2012-12-08 22:13:06 +0300

    Add correct path for recording file

--- src/com/eucsoft/beeper/audio/AudioRecorder.java
+++ src/com/eucsoft/beeper/audio/AudioRecorder.java
@@ -1,5 +1,6 @@
 package com.eucsoft.beeper.audio;
 
+import java.io.File;
 import java.io.IOException;
 
 import android.media.MediaRecorder;
@@ -15,13 +16,23 @@ public class AudioRecorder {
 		recorder = new MediaRecorder();
 		recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
 		
-		recorder.setOutputFile("test");
+		File recordDir = new File(android.os.Environment.getExternalStorageDirectory().getAbsolutePath() + "/record");
+		if (!recordDir.exists()) {
+			recordDir.mkdir();
+		}
+		
+		String filePath = recordDir.getAbsolutePath();
+		filePath += "rec.3gp";
+		
+		
 		recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+		recorder.setOutputFile(filePath);
 		recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
 
 		try {
 			recorder.prepare();
 		} catch (IOException e) {
+			e.printStackTrace();
 		}
 
 		recorder.start();
