8a0ae3ac1a358104917031c5863cb51186cf2de5
by-ktif <monovar@tut.by>
2012-12-08 10:37:38 -0430

    Make background for record button

--- src/com/eucsoft/beeper/button/record/RecordButtonListener.java
+++ src/com/eucsoft/beeper/button/record/RecordButtonListener.java
@@ -1,13 +1,25 @@
 package com.eucsoft.beeper.button.record;
 
+import com.eucsoft.beeper.R;
+
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class RecordButtonListener implements OnClickListener {
 
 	@Override
 	public void onClick(View v) {
-		v.setBackgroundColor(0);
+		Button b = ((Button)v);
+		String text = b.getText().toString();
+		int i = 0;
+		try {
+			i = Integer.parseInt(text);
+		} catch(Exception e) {
+			i = 0;
+		}
+		b.setText(String.valueOf(++i));
+		//b.setBackgroundResource(R.drawable.record_button_pressed);
 	}
 	
 	
