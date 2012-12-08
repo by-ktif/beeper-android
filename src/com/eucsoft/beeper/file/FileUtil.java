04347177163d1d6759e86fb0121da9d492956a9b
dimhold <dimhold@gmail.com>
2012-12-09 02:27:35 +0300

    Implement FileUtil class

--- src/com/eucsoft/beeper/file/FileUtil.java
+++ src/com/eucsoft/beeper/file/FileUtil.java
@@ -1,20 +1,47 @@
 package com.eucsoft.beeper.file;
 
 import java.io.File;
+import java.util.Random;
+
+import android.os.Environment;
 
 public class FileUtil {
 	
-	public String getUniqFilePath() {
-		
+	public static String getUniqFilePath() {
+		File beeperDir = getBeeperFolder();
+		String uniqFilePath = beeperDir.getAbsolutePath() + "/" + generateUniqName();
+		File uniqFile = new File(uniqFilePath);
+		if (uniqFile.exists()) {
+			return getUniqFilePath();
+		}
+		return uniqFile.getAbsolutePath();
 	}
 	
-	public String deleteFile(String filePath) {
+	private static File getBeeperFolder() {
+		//TODO: move hardcoded beeper folder to ApplicationContext
+		File beeperDir = new File(Environment.getExternalStorageDirectory() + "/beeper");
+        if (!beeperDir.exists()) {
+        	beeperDir.mkdir();
+        }
+        return beeperDir;
+	}
+	
+	private static String generateUniqName() {
+		Random random = new Random();
+		String randomSalt = Integer.toString(random.nextInt());
+		String time = Long.toString(System.currentTimeMillis());
+		return time + randomSalt;
+	}
+	
+	public static void deleteFile(String filePath) {
 		File file = new File(filePath);
 		deleteFile(file);
 	}
 	
-	public String deleteFile(File file) {
-		
+	public static void deleteFile(File file) {
+		if (file.exists()) {
+			file.delete();
+		}
 	}
 	
 
