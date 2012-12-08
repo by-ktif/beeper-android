2036cc09c90d91e945b0b0da2b6ddc1f8a4e9532
viktar <viktar@upmygame.com>
2012-12-08 22:26:21 +0300

    Download and Post mockup

---
+++ src/com/eucsoft/beeper/http/POSTRequestTask.java
@@ -0,0 +1,46 @@
+package com.eucsoft.beeper.http;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.os.AsyncTask;
+
+public class POSTRequestTask extends AsyncTask<NameValuePair, Void, Void> {
+
+	@Override
+	protected Void doInBackground(NameValuePair... params) {
+		// Create a new HttpClient and Post Header
+	    HttpClient httpclient = new DefaultHttpClient();
+	    HttpPost httppost = new HttpPost("http://www.yoursite.com/script.php");
+
+	    try {
+	        // Add your data
+	        List<NameValuePair> nameValuePairs = Arrays.asList(params);// new ArrayList<NameValuePair>(2);
+	        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+	        // Execute HTTP Post Request
+	        HttpResponse response = httpclient.execute(httppost);
+	        
+	    } catch (ClientProtocolException e) {
+	        // TODO Auto-generated catch block
+	    } catch (IOException e) {
+	        // TODO Auto-generated catch block
+	    }
+	    
+	    return null;
+
+	}
+
+	
+	
+
+}
