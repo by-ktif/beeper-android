acfde788149499230450c6fe992920bf565a6f9f
dimhold <dimhold@gmail.com>
2012-12-09 01:46:53 +0300

    Add FileUtil stub

---
+++ src/com/eucsoft/beeper/file/FileUtil.java
@@ -0,0 +1,21 @@
+package com.eucsoft.beeper.file;
+
+import java.io.File;
+
+public class FileUtil {
+	
+	public String getUniqFilePath() {
+		
+	}
+	
+	public String deleteFile(String filePath) {
+		File file = new File(filePath);
+		deleteFile(file);
+	}
+	
+	public String deleteFile(File file) {
+		
+	}
+	
+
+}
