282bcc96c231a9bd7e099fe97a0e82f7409382d4
dimhold <dimhold@gmail.com>
2013-02-17 18:27:56 +0300

    Add first APITest mockup

---
+++ test/com/eucsoft/beeper/api/APITest.java
@@ -0,0 +1,15 @@
+package com.eucsoft.beeper.api;
+
+import android.test.AndroidTestCase;
+
+public class APITest extends AndroidTestCase {
+	
+	public void connectTest() {
+		assertTrue(false);
+	}
+	
+	public void changeRoomTest() {
+		assertTrue(false);
+	}
+
+}
